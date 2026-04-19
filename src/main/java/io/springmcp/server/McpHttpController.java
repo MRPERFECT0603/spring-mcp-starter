93a42211f07f2b46457ca21554428965eb65c35b
Vivek Shaurya <vivekshaurya62@gmail.com>
2026-04-19 19:46:01 +0530

    Added the crossOrigin

--- src/main/java/io/springmcp/server/McpHttpController.java
+++ src/main/java/io/springmcp/server/McpHttpController.java
@@ -12,6 +12,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/mcp")
 public class McpHttpController {
